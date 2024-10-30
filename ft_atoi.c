653f63464494b0b57f763006972f1bf51142d669
Mohamed Taha Gamraoui <mgamraou@e2r9p4.1337.ma>
2024-10-30 11:01:56 +0100

    semiatoi

---
+++ ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgamraou <mgamraou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 09:42:18 by mgamraou          #+#    #+#             */
+/*   Updated: 2024/10/30 10:22:21 by mgamraou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+int ft_atoi(const char *str)
+{
+    long long num;
+    int i;
+    int sign;
+    
+    num = 0;
+    i = 0;
+    sign = 0;
+    
+    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if(str[i] == '+' || str[i] == '-')
+    {
+        if(str[i] == '-')
+            sign = 1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        num = (num * 10) + (str[i] - '0');
+        i++;
+    }
+    if (sign == 1)
+        return (num * -1);    
+    return (num);
+}
+int main()
+{
+    char *s1 = "    -92233720368547758079786";
+    printf("%i\n", atoi(s1));
+    printf("%i", ft_atoi(s1));
+}
