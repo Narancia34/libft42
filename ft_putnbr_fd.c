ab1e0a8912aeb6f6f4bae813a6c4fac329057baf
Mohamed Taha Gamraoui <mgamraou@e2r8p2.1337.ma>
2024-11-01 12:07:03 +0100

    semidone

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgamraou <mgamraou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 11:58:17 by mgamraou          #+#    #+#             */
+/*   Updated: 2024/11/01 12:02:50 by mgamraou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+void ft_putnbr_fd(int n, int fd)
+{
+    long num;
+
+    num = n;
+    if(num < 0)
+    {
+        ft_putchar_fd('-', fd);
+        num *= -1;
+    }
+    if(num > 9)
+        ft_putnbr_fd(num / 10, fd);
+    ft_putchar_fd(num % 10, fd);
+}
