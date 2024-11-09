feef8f1ac85be2a5ca2f464c66321964f8b40690
Mohamed Taha Gamraoui <mgamraou@c4r2p7.1337.ma>
2024-11-09 17:09:25 +0100

    libft

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,27 +6,33 @@
 /*   By: mgamraou <mgamraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 17:22:51 by mgamraou          #+#    #+#             */
-/*   Updated: 2024/11/09 12:07:36 by mgamraou         ###   ########.fr       */
+/*   Updated: 2024/11/09 17:08:06 by mgamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *s1, const char *s2, size_t n)
+size_t    ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	j;
+    size_t    s;
+    size_t    d;
+    size_t    di;
+    size_t    si;
 
-	i = 0;
-	while (s1[i] != 0 && i < n)
-		i++;
-	j = i;
-	while (s2[i - j] != 0 && i < n - 1)
-	{
-		s1[i] = s2[i - j];
-		i++;
-	}
-	if (j < n)
-		s1[i] = 0;
-	return (j + ft_strlen(s2));
+    si = ft_strlen(src);
+    if (!dst && size == 0)
+        return (si);
+    d = ft_strlen(dst);
+    di = d;
+    if (size <= di)
+        return (size + si);
+    s = 0;
+    while (src[s] && d + 1 < size)
+    {
+        dst[d] = src[s];
+        s++;
+        d++;
+    }
+    dst[d] = 0;
+    return (di + si);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,30 +6,31 @@
 /*   By: mgamraou <mgamraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 14:41:21 by mgamraou          #+#    #+#             */
-/*   Updated: 2024/11/09 12:06:24 by mgamraou         ###   ########.fr       */
+/*   Updated: 2024/11/09 17:07:49 by mgamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char    *ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	i;
-	size_t	j;
+    size_t    i;
+    size_t    j;
 
-	i = 0;
-	j = ft_strlen(little);
-	if (!little)
-	{
-		return ((char *)big);
-	}
-	while (big[i] != 0 && len-- >= j)
-	{
-		if (ft_strncmp(&big[i], little, j) == 0)
-		{
-			return ((char *)&big[i]);
-		}
-		i++;
-	}
-	return (NULL);
+    i = 0;
+    if (little[0] == '\0')
+        return ((char *)(big + i));
+    while (i < len && big[i] != '\0')
+    {
+        j = 0;
+        if (big[i] == little[j])
+        {
+            while (little[j] && (i + j) < len && big[i + j] == little[j])
+                j++;
+            if (little[j] == '\0')
+                return ((char *)(big + i));
+        }
+        i++;
+    }
+    return (NULL);
 }
