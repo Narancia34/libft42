69eca958125d0cecfc9a4f675b0db0595a24df4e
Mohamed Taha Gamraoui <mgamraou@c4r4p7.1337.ma>
2024-11-11 10:59:42 +0100

    libft

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,29 +6,30 @@
 /*   By: mgamraou <mgamraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 14:41:21 by mgamraou          #+#    #+#             */
-/*   Updated: 2024/11/11 10:54:09 by mgamraou         ###   ########.fr       */
+/*   Updated: 2024/11/11 10:59:35 by mgamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
-	if (needle[0] == '\0')
-		return ((char *)(haystack + i));
-	while (haystack[i] != '\0' && i < len)
+	if (little[0] == '\0')
+		return ((char *)(big + i));
+	while (i < len && big[i] != '\0')
 	{
-		if (haystack[i] == needle[0])
+		j = 0;
+		if (big[i] == little[j])
 		{
-			j = 0;
-			while (needle[j] && haystack[i + j] == needle[j] && (i + j) < len)
+			while (little[j] && (i + j) < len && big[i + j] == little[j])
 				j++;
-			if (needle[j] == '\0')
-				return ((char *)(haystack + i));
+			if (little[j] == '\0')
+				return ((char *)(big + i));
 		}
 		i++;
 	}
